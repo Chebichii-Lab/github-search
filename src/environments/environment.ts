6935796924997c31c2c3d290e97fbf7d2ac628db
Chebichii-Lab <natasha.jebichii@student.moringaschool.com>
2021-05-21 18:19:04 +0300

    added changes in the environment.ts file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  api_key:"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",
 };
 
 /*
