c38b1b593d0d0027c59fc6ec157a0ffd4f855a9a
Chebichii-Lab <natasha.jebichii@student.moringaschool.com>
2021-05-21 21:06:00 +0300

    added changes in the file

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-
+  username: string                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+  @Output() emitUserName = new EventEmitter<string>()
+  search(username){
+    this.emitUserName.emit(username)
+  }
   constructor() { }
 
   ngOnInit(): void {
