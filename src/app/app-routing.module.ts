6a7f108a3cc8899cc2484ed8593c4f757f4eb768
Chebichii-Lab <natasha.jebichii@student.moringaschool.com>
2021-05-23 12:59:13 +0300

    added changes in the file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RepoComponent } from './repository/repository.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component:  HomePageComponent},
+  { path: 'repositories', component: RepoComponent },
+  { pathMatch: 'full', redirectTo: 'home', path: '' },
+  { path: '**', component: NotFoundComponent },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
